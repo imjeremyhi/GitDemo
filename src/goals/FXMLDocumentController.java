ca58fd08993e3648aed53c2c12897c2f143b8cfe
Aaron Li <Aaron Li>
2016-04-22 09:59:04 +1000

    1st Commit

---
+++ src/goals/FXMLDocumentController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package goals;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Aaron
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private void handleButton1(ActionEvent event) throws IOException {
+        System.out.println("Set goals");
+        Parent root = FXMLLoader.load(getClass().getResource("Scene2.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = Goals.getStage();
+        stage.setScene(scene);
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
